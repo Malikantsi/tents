5449276c76307b5bffccb96f6ccf35be9aca1589
malikantsi <lekhoanalucy@gmail.com>
2025-08-02 01:24:26 +0200

    Added a few thing

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,7 +1,7 @@
 import { renderListWithTemplate } from './utils.mjs';
 
 function productCardTemplate(product) {
-    console.log("Rendering product:", product.Name);
+    // console.log("Rendering product:", product.Name);
     let discountHTML = "";
 
     if (product.FinalPrice < product.SuggestedRetailPrice) {
@@ -11,16 +11,21 @@ function productCardTemplate(product) {
         <span class="discount-badge">Save $${discountAmount}</span>
       `;
     }
+
     return `<li class="product-card">
         <a href="product_pages/index.html?product=${product.Id}">
-        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
         <h2>${product.Brand.Name}</h2>
         <h3>${product.Name}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
         ${discountHTML}
       </a>
       </li>`;
+
 }
+
+
+
 
 export default class ProductList {
     constructor(category, dataSource, listElement) {
@@ -28,16 +33,18 @@ export default class ProductList {
         this.dataSource = dataSource;
         this.listElement = listElement;
     }
-
     async init() {
-        const list = await this.dataSource.getData();
+        const list = await this.dataSource.getData(this.category);
         this.renderList(list);
     }
-
+    async findProductByName(searchString) {
+        const list = await this.dataSource.findProductByName(searchString, this.category);
+        this.listElement.innerHTML = "";
+        this.renderList(list);
+    }
     renderList(list) {
         console.log("Rendering list with", list.length, "items");
 
         renderListWithTemplate(productCardTemplate, this.listElement, list);
-
     }
 }
